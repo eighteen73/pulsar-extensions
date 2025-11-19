9221ccb565da97e8944c883a55d1f39fb527b4c8
Brett Mason <brett@eighteen73.co.uk>
2025-11-19 12:23:48 +0000

    Sort order for extensions

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 module.exports = {
 	...defaultConfig,
 	entry: {
-		'group/sticky': './src/group/sticky/index.js',
-		'group/link': './src/group/link/index.js',
-		'group/row': './src/group/row/index.js',
-		'group/grid': './src/group/grid/index.js',
 		'column/order': './src/column/order/index.js',
 		'columns/stacked': './src/columns/stacked/index.js',
+		'group/grid': './src/group/grid/index.js',
+		'group/link': './src/group/link/index.js',
+		'group/row': './src/group/row/index.js',
+		'group/sticky': './src/group/sticky/index.js',
 	},
 };
