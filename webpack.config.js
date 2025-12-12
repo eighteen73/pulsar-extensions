4b55101979e620ca97d3ae93150ed3a015ec08c4
Brett Mason <brett@eighteen73.co.uk>
2025-12-12 09:21:09 +0000

    Allow including view.js files for extensions for any front end functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,35 @@
+const path = require('path');
+const fg = require('fast-glob');
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+
+const getEntryName = (file) => {
+	const cleaned = file.replace(/^src\//, '').replace(/\.js$/, '');
+
+	if (cleaned.endsWith('/view')) {
+		return cleaned.replace(/\/view$/, '-view');
+	}
+
+	return cleaned.replace(/\/index$/, '');
+};
+
+const buildEntries = () => {
+	const files = fg.sync(['src/**/index.js', 'src/**/view.js'], {
+		onlyFiles: true,
+	});
+
+	return files.reduce(
+		(entries, file) => ({
+			...entries,
+			[getEntryName(file)]: path.resolve(__dirname, file),
+		}),
+		{}
+	);
+};
 
 module.exports = {
 	...defaultConfig,
 	entry: {
 		'editor/register-icons': './src/editor/register-icons.js',
-		'button/icon': './src/button/icon/index.js',
-		'column/order': './src/column/order/index.js',
-		'columns/stacked': './src/columns/stacked/index.js',
-		'group/grid': './src/group/grid/index.js',
-		'group/link': './src/group/link/index.js',
-		'group/row': './src/group/row/index.js',
-		'group/sticky': './src/group/sticky/index.js',
+		...buildEntries(),
 	},
 };
